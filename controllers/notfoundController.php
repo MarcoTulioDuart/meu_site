e0ec7b6bca7587b1d78611044314871b11ade30d
Geovanna <geovanna.fantuzzi@gmail.com>
2023-08-28 15:12:44 -0300

    remoção de sessions em notfound controller 28/08/23

--- controllers/notfoundController.php
+++ controllers/notfoundController.php
@@ -15,32 +15,7 @@ class notfoundController extends Controller
         $accounts = new accounts();
         $id = $_SESSION['proTSA_online'];
         $data['info_user'] = $accounts->get($id);
-        //Session de projeto
-        if (isset($_SESSION['project_protsa'])) {
-            unset($_SESSION['project_protsa']);
-        }
-        //Session do Primeiro Módulo
-        if (isset($_SESSION['integration_id_proTSA'])) {
-            unset($_SESSION['integration_id_proTSA']);
-        }
-        //Session do Segundo Módulo
 
-        //Session do Terceiro Módulo
-        if (isset($_SESSION['signals_id_proTSA'])) {
-            unset($_SESSION['signals_id_proTSA']);
-            unset($_SESSION['project_signals_id_proTSA']);
-        }
-        //Session do Quarto Módulo
-        if (isset($_SESSION['parameters_id_proTSA'])) {
-            unset($_SESSION['parameters_id_proTSA']);
-            unset($_SESSION['parameters_project_id_proTSA']);
-        }
-        //Session do Quinto Módulo
-
-        //Session do Sexto Módulo
-        if (isset($_SESSION['safe_test_id_proTSA'])) {
-            unset($_SESSION['safe_test_id_proTSA']);
-        }
         //fim do básico
 
         $this->loadTemplate("home", "notfound", $data);
